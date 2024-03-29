3fc46196272d79bcbc1f3ed8b3e8f2db490d4f7f
AyushThakur79 <thakurayush5440@gmail.com>
2024-03-29 08:22:57 +0530

    commit

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-	let loaderNum = document.querySelector(".loaderNum");
-	let current = 0;
+let loaderNum = document.querySelector(".loaderNum");
+let current = 0;
 
-	function updateLoading() {
-		if (current === 100) {
-			return;
-		}
-
-		current += Math.floor(Math.random() * 10) + 1;
-
-		if (current > 100) {
-			current = 100;
-		}
-
-		loaderNum.textContent = current + "%";
-		let delay = Math.floor(Math.random() * 200) + 50;
-		setTimeout(updateLoading, delay);
+function updateLoading() {
+	if (current === 100) {
+		return;
 	}
-	updateLoading();
 
-gsap.to(".loaderNum",{
+	current += Math.floor(Math.random() * 10) + 1;
+
+	if (current > 100) {
+		current = 100;
+	}
+
+	loaderNum.textContent = current + "%";
+	let delay = Math.floor(Math.random() * 200) + 50;
+	setTimeout(updateLoading, delay);
+}
+updateLoading();
+
+gsap.to(".loaderNum", {
 	delay: 3.2,
 	opacity: 0,
 });
 
-gsap.to(".dark1 p",{
+gsap.to(".dark1 p", {
 	opacity: 0,
-    duration: 0.4,
-    ease: Power4,
-    yoyo: true,
-    repeat: 6,
+	duration: 0.4,
+	ease: Power4,
+	yoyo: true,
+	repeat: 6,
 });
 
-gsap.from("#loaderImage",{
-    opacity: 0,
-    duration: 0.9,
-    ease: Power4,
-    yoyo: true,
-    repeat: 3,
+gsap.from("#loaderImage", {
+	opacity: 0,
+	duration: 0.9,
+	ease: Power4,
+	yoyo: true,
+	repeat: 3,
 });
-
 
 gsap.to(".box", 1.5, {
 	delay: 3,
@@ -50,6 +49,8 @@ gsap.to(".box", 1.5, {
 });
 
 gsap.to(".loader", {
-    delay: 5,
-    y: "-=100%"
+	delay: 5,
+	y: "-=100%",
 });
+
+console.log("hello world");
