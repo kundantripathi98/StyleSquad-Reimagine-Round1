4e304794651e4ef91f85b4452980344719cd6304
AyushThakur79 <thakurayush5440@gmail.com>
2024-03-30 21:25:05 +0530

    navbar

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-	let loaderNum = document.querySelector(".loaderNum");
-	let current = 0;
+let loaderNum = document.querySelector(".loaderNum");
+let current = 0;
 
-	function updateLoading() {
-		if (current === 100) {
-			return;
-		}
-
-		current += Math.floor(Math.random() * 10) + 1;
-
-		if (current > 100) {
-			current = 100;
-		}
-
-		loaderNum.textContent = current + "%";
-		let delay = Math.floor(Math.random() * 200) + 50;
-		setTimeout(updateLoading, delay);
+function updateLoading() {
+	if (current === 100) {
+		return;
 	}
-	updateLoading();
 
-gsap.to(".loaderNum",{
+	current += Math.floor(Math.random() * 10) + 1;
+
+	if (current > 100) {
+		current = 100;
+	}
+
+	loaderNum.textContent = current + "%";
+	let delay = Math.floor(Math.random() * 200) + 50;
+	setTimeout(updateLoading, delay);
+}
+updateLoading();
+
+gsap.to(".loaderNum", {
 	delay: 3.2,
 	opacity: 0,
 });
 
-gsap.to(".dark1 p",{
+gsap.to(".dark1 p", {
 	opacity: 0,
-    duration: 0.4,
-    ease: Power4,
-    yoyo: true,
-    repeat: 6,
+	duration: 0.4,
+	ease: Power4,
+	yoyo: true,
+	repeat: 6,
 });
 
-gsap.from("#loaderImage",{
-    opacity: 0,
-    duration: 0.9,
-    ease: Power4,
-    yoyo: true,
-    repeat: 3,
+gsap.from("#loaderImage", {
+	opacity: 0,
+	duration: 0.9,
+	ease: Power4,
+	yoyo: true,
+	repeat: 3,
 });
-
 
 gsap.to(".box", 1.5, {
 	delay: 3,
@@ -50,6 +49,29 @@ gsap.to(".box", 1.5, {
 });
 
 gsap.to(".loader", {
-    delay: 5,
-    y: "-=100%"
+	delay: 5,
+	y: "-=100%",
+});
+
+// Rolling Text Effect
+
+let rolling = document.querySelectorAll(".text-rolling");
+rolling.forEach((element) => {
+	let innerText = element.innerText;
+	element.innerHTML = "";
+	let textContainer = document.createElement("div");
+	textContainer.classList.add("block");
+	for (let letter of innerText) {
+		let span = document.createElement("span");
+		span.innerText = letter.trim() === "" ? "\xa0" : letter;
+		span.classList.add("letter");
+		textContainer.appendChild(span);
+	}
+	element.appendChild(textContainer);
+	element.appendChild(textContainer.cloneNode(true));
+});
+rolling.forEach((element) => {
+	element.addEventListener("mousemove", () => {
+		element.classList.remove("play");
+	});
 });
