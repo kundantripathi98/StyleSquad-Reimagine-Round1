022ce5c82408fa356d8956fe2f7c0b590f5bcf39
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-03-30 21:24:07 +0530

    Navbar created

--- script.js
+++ script.js
@@ -53,3 +53,51 @@ gsap.to(".loader", {
 	y: "-=100%",
 });
 
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+	const menu = document.querySelector(".menu");
+	const toggleBtn = document.querySelector(".toggle");
+	const closeBtn = document.querySelector(".title");
+	let isOpen = false;
+
+	const timeline = gsap.timeline({paused: true});
+
+	timeline.to(menu, {
+		duration: 0.3,
+		opacity: 1
+	});
+
+	timeline.to(menu, {
+		duration: 1,
+		ease: "power3.inOut",
+		clipPath: "polygon(49.75% 0%, 50.25% 0%, 50.25% 100%, 49.75% 100%)"
+	},"-=0.3");
+
+	timeline.to(menu, {
+		duration: 1,
+		ease: "power3.inOut",
+		clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+		pointerEvents: "all"
+	});
+
+	toggleBtn.addEventListener("click", ()=>{
+		if(isOpen){
+			timeline.reverse();
+		} else{
+			timeline.play();
+		}
+
+		isOpen = !isOpen;
+	});
+
+	closeBtn.addEventListener("click", ()=>{
+		if(isOpen){
+			timeline.reverse();
+		} else{
+			timeline.play();
+		}
+
+		isOpen = !isOpen;
+	});
+})
