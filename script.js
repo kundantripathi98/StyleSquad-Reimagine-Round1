413284c8126b0f4542a3b0a598a3fdd87e08788f
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-06-13 16:16:04 +0530

    updated

--- script.js
+++ script.js
@@ -183,6 +183,16 @@ function carSliderAnimation(){
 	document.querySelector(".slider").addEventListener('input', (e) => {
 		container.style.setProperty('--position', `${e.target.value}%`);
 	});
+
+	container.addEventListener("mouseenter", ()=>{
+		document.querySelector(".mousefollower").style.opacity = 0;
+		document.querySelector(".mousefollower").style.transition = 'all cubic-bezier(0.19, 1, 0.22, 1) 1s';
+	});
+
+	container.addEventListener("mouseleave", ()=>{
+		document.querySelector(".mousefollower").style.opacity = 1;
+		document.querySelector(".mousefollower").style.transition = 'all cubic-bezier(0.19, 1, 0.22, 1) 1s';
+	});
 }
 
 function cursor(){
