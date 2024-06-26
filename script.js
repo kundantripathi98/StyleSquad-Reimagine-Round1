d55c4fc3cb621a56d087440c6ce51258f13bcf4c
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-06-26 17:40:06 +0530

    updated

--- script.js
+++ script.js
@@ -381,7 +381,7 @@ function thirdSectionAnimation(){
         project.style.color = "rgb(80,74,69)";
         execution.style.color = "rgb(80,74,69)";
         para.textContent = "Our team works with our clients to refine an idea and concept into an executable design. We create a final design that encompasses the brand narrative to bring stories to life and provide end-to-end design solutions from concept, design, and architectural drawings to 3D renderings.";
-        image.src = "images/project-banner.webp";
+        image.src = "./assets/images/news/news1.webp";
         gsap.from(".para",{
             opacity: 0,
             duration: 0.5,
@@ -402,7 +402,7 @@ function thirdSectionAnimation(){
         design.style.color = "rgb(80,74,69)";
         execution.style.color = "rgb(80,74,69)";
         para.textContent = "Once we have a design, our production team takes the lead in bringing it to life. We manage all stages of the project, from build specifications and technical drawings to site surveys, vendor management, and 2D & 3D production. We have an extensive network of partners to meet each unique design and project need.";
-        image.src = "images/Project-banner2.webp";
+        image.src = "./assets/images/news/news5.webp";
         gsap.from(".para",{
             opacity: 0,
             duration: 0.5,
@@ -423,7 +423,7 @@ function thirdSectionAnimation(){
         project.style.color = "rgb(80,74,69)";
         design.style.color = "rgb(80,74,69)";
         para.textContent = "We’re with you every step of the way, from the project initiation to launch day. Our production and design teams are onsite to direct and guide the process down to the last point of completion, ensuring success across the built space and experience.";
-        image.src = "images/Project-banner3.webp";
+        image.src = "./assets/images/news/news3.jpg";
         gsap.from(".para",{
             opacity: 0,
             duration: 0.5,
